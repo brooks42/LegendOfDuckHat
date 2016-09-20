e4b5eda64d1b9aabee3259f705c795af580fd0dc
brooks42 <britainsux@gmail.com>
2016-09-20 04:40:11 -0700

    Add the initial GameState class, and start in on the states package.

---
+++ states/states.py
@@ -0,0 +1,18 @@
+
+"""
+The default GameState superclass. Gamestates stack on top of eachother to provide
+the UI, effects etc for the game.
+"""
+class GameState:
+
+    def __init__(self):
+        #creates a default state
+        pass
+
+    def update(deltaT):
+        # updates the game state with the passed deltaT float
+        pass
+
+    def render(): # might need to take a parameter? multiple render methods?
+        # renders this game state to the screen
+        pass
