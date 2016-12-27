e6b382db258c4e2619f92c372f82fcdb285e8f22
Chris <brooks42@github.com>
2016-12-27 00:32:21 -0800

    Add some to the Mob class

--- mobs/mobsdefs.py
+++ mobs/mobsdefs.py
@@ -1,2 +1,14 @@
 
 import pygame
+
+# defines a set of information that's used by all mobs in the game
+class Mob(Object):
+
+    # initialize a mob
+    def __init__(self, sprite):
+        self.sprite = sprite;
+        self.position = position;
+
+    # render the mob
+    def render(screen):
+        self.sprite.render(screen)
